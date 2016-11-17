22cee4dbc0ead36700aef2b45ea4785efec44a8d
Salem Ozaki <sozaki19@csl19-l.cornellcollege.edu>
2016-11-17 16:51:25 -0600

    still working on Assembler

--- projects/06/Assembler.java
+++ projects/06/Assembler.java
@@ -9,6 +9,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.io.PrintStream;
+import java.util.Hashtable;
 import java.util.Scanner;
 
 
@@ -18,13 +19,18 @@ import java.util.Scanner;
  */
 public class Assembler {
     String instruction;
+    Hashtable<String,String> destTable;
+    
     public Assembler(String args) throws Exception{
-        parse(args);
+        destTable = loading();
+        String outPrefix = args.substring(0, args.indexOf("."));
+        PrintStream out = new PrintStream(new File(outPrefix + ".hack"));
+        parse(args,out);
     }
     
-    private void parse(String args) throws Exception{
+    private void parse(String args, PrintStream out) throws Exception{
         String filename = args;
-        try{
+        //try{
             Scanner in = new Scanner(new File(filename));
             while(in.hasNextLine()){
                 String line = in.nextLine();
@@ -34,39 +40,75 @@ public class Assembler {
                 } else {
                     System.out.println(spaceless);
                     assembly(spaceless);
+                    out.println(instruction);
                 }
             
             }
-        } catch(Exception e) {
+        //} catch(Exception e) {
             System.out.println("I am sorry, but we cannot find the file you"
                     + " are trying to access.");
-        }
+        //}
     }
     
     private void assembly(String line){
-        if(line.startsWith("@")){
-            aInstruction(line);
+        String nline = line;
+        if(line.contains("/")){
+            nline = line.substring(0, line.indexOf("/"));
+        }
+        
+        if(nline.startsWith("@")){
+            aInstruction(nline);
         } else {
-            cInstruction(line);
+            cInstruction(nline);
         }
     }
     
     private void aInstruction(String aline){
         instruction = "0";
-        System.out.println(instruction);
-        System.out.println("I will do this later"); // Continue this later!!!!
-        System.out.println("");
+        String line = aline.substring(1, aline.length()); // gets rid of the @.
+        System.out.println(line + " this is the command for A- instruction");
+        System.out.println(destTable.get(line) + " is the location for "
+                + line + ".");
+        /*System.out.println("Does destTable contain " + line +"?  "
+        + destTable.containsKey(line) + ".");   This is used to test
+        destTable.*/
+        
+        String addr = null;  // incase the line does not meet the if statement
+        if(destTable.containsKey(line)){ // checking the table
+            String location = destTable.get(line);
+            int num = Integer.parseInt(location);
+            addr = Integer.toBinaryString(num);
+        } else {
+            int num = Integer.parseInt(line);
+            addr = Integer.toBinaryString(num);
+        }
+        while(addr.length() != 15){
+            addr = "0" + addr;
+        }
+        instruction = instruction + addr;
+        System.out.println(instruction + " is the binary location of " + line + ".");
     }
     
     private void cInstruction(String cline){
         instruction = "111";
+        System.out.println(cline + " this should not have //");
+            
             // Doing computation
             comp(cline);
             // Doing the destination now
-            destination(cline);
+            if(cline.contains("=")){
+                // destination will only looks at the left of the equals.
+                destination(cline);
+            } else {
+                instruction = instruction + "000";
+            }
             // When you give a value to a memory, you do not jump
-            jump(cline);
-            instruction = instruction + "000";
+            System.out.println(cline.contains(";") + " does it contain ;?");
+            if(cline.contains(";")){
+                jump(cline);
+            } else {
+                instruction = instruction + "000";
+            }
             System.out.println(instruction + " this is the final instruction!");
         
     }
@@ -93,21 +135,17 @@ public class Assembler {
             d3 = "0";
         }
         instruction = instruction + d1 + d2 + d3;
+        System.out.println(instruction + " this has destination");
     }
     
     private void comp(String line){
-        String compare;
-            /* This may not be necessary
-        int stop = line.indexOf("/"); // finds where the comment starts.
-        int start = line.indexOf("="); // finds where to start.
-            */
-            if(line.contains("/")){
-                compare = line.substring(line.indexOf("=") + 1, line.indexOf("/")); /* because it needs
-            start a string after the = sign. */
-            } else {
+        String compare = line;
+            if(line.contains("=")){
                 compare = line.substring(line.indexOf("=") + 1);
+                System.out.println(compare + " should not contain =");
             }
-            if(line.contains(";")){
+            if(line.contains(";")){ /* gets rid of the jump because this is
+                comp not jump. */
                 compare = compare.substring(0, compare.indexOf(";"));
             }
             System.out.println(compare + " This is the thing that will be computed");
@@ -219,8 +257,59 @@ public class Assembler {
                     + " when a = 1, means that we are dealing with M.");
         }
     }
+    
     private void jump(String line){
-        
+        String jum = line.substring(line.indexOf(";") + 1, line.length());
+        System.out.println(jum + " this should be just the jump");
+        if(jum.equals("JGT")){
+            instruction = instruction + "001";
+        } else if(jum.equals("JEQ")){
+            instruction = instruction + "010";
+            
+        } else if(jum.equals("JGE")){
+            instruction = instruction + "011";
+            
+        } else if(jum.equals("JLT")){
+            instruction = instruction + "100";
+            
+        } else if(jum.equals("JNE")){
+            instruction = instruction + "101";
+            
+        } else if(jum.equals("JLE")){
+            instruction = instruction + "110";
+            
+        } else if(jum.equals("JMP")){
+            instruction = instruction + "111";
+            
+        }
+    }
+    
+    private Hashtable loading(){
+        destTable = new Hashtable<String, String>();
+        destTable.put("SP", "0");
+        destTable.put("LCL", "1");
+        destTable.put("ARG", "2");
+        destTable.put("THIS", "3");
+        destTable.put("THAT", "4");
+        destTable.put("R0", "0");
+        destTable.put("R1", "1");
+        destTable.put("R2", "2");
+        destTable.put("R3", "3");
+        destTable.put("R4", "4");
+        destTable.put("R5", "5");
+        destTable.put("R6", "6");
+        destTable.put("R7", "7");
+        destTable.put("R8", "8");
+        destTable.put("R9", "9");
+        destTable.put("R10", "10");
+        destTable.put("R11", "11");
+        destTable.put("R12", "12");
+        destTable.put("R13", "13");
+        destTable.put("R14", "14");
+        destTable.put("R15", "15");
+        destTable.put("SCREEN", "16384");
+        destTable.put("KBD", "24576");
+        return(destTable);
     }
 
     /**
