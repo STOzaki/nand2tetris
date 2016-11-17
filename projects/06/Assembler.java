9ab10d3b73e047e6d83ce61d28fb2ca0ef6dc966
Salem Ozaki <sozaki19@csl19-l.cornellcollege.edu>
2016-11-17 01:02:07 -0600

    Assembler in progress

--- projects/06/Assembler.java
+++ projects/06/Assembler.java
@@ -17,13 +17,8 @@ import java.util.Scanner;
  * @author sozaki19
  */
 public class Assembler {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) throws Exception {
-        String filename = args[0];
-        System.out.println(filename);
+    public Assembler(String args) throws Exception{
+        String filename = args;
         Scanner in = new Scanner(new File(filename));
         while(in.hasNextLine()){
             String line = in.nextLine();
@@ -32,7 +27,33 @@ public class Assembler {
                 
             } else {
                 System.out.println(spaceless);
+                assembly(spaceless);
             }
+        
+        }
+    }
+    
+    private void assembly(String line){
+        if(line.startsWith("@")){
+            aInstruction();
+        } else {
+            cInstruction();
+        }
+    }
+    
+    private void aInstruction(){
+        
+    }
+    
+    private void cInstruction(){
+        
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception {
+        Assembler assem = new Assembler(args[0]);
             
             /*else if(!(in.hasNext())) {
                 System.out.println("Deleted");
@@ -41,5 +62,3 @@ public class Assembler {
             } */
         } 
     }
-    
-}
